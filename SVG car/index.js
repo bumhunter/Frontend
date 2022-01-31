f00f82127f5e2e6acdb784794a8677a7fe2d538a
bumhunter <87677053+bumhunter@users.noreply.github.com>
2022-01-31 11:41:16 +0300

    при нажатии машина едет вправо

---
+++ SVG car/index.js
@@ -0,0 +1,15 @@
+function changeBG1() {
+    document.querySelector("#wheel-border1").style.fill = "red";
+}
+
+function changeBG2() {
+    document.querySelector("#wheel-border2").style.fill = "red";
+}
+
+function moveForward() {
+    document.querySelector("#car").style.marginLeft = "500px";
+}
+
+function moveBackward() {
+    document.querySelector("#car").style.marginLeft = "0px";
+}
